aedf6087e5c6a1f17946a26d00cef7ce1ca5aecb
Nick <nick@telsanstudios.com>
2022-09-25 16:33:30 -0400

    [#2] Authentication (#7)
    - Adds basic authentication
    - Adds basic home screen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
-import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
-import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
-
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
 import * as theme from 'config/chakra.config'
+
+import { AuthProvider } from '@redwoodjs/auth'
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import './scaffold.css'
 import './index.css'
 
 const extendedTheme = extendTheme(theme)
@@ -14,12 +16,14 @@ const extendedTheme = extendTheme(theme)
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <ColorModeScript />
-      <ChakraProvider theme={extendedTheme}>
-        <RedwoodApolloProvider>
-          <Routes />
-        </RedwoodApolloProvider>
-      </ChakraProvider>
+      <AuthProvider type="dbAuth">
+        <ColorModeScript />
+        <ChakraProvider theme={extendedTheme}>
+          <RedwoodApolloProvider>
+            <Routes />
+          </RedwoodApolloProvider>
+        </ChakraProvider>
+      </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
