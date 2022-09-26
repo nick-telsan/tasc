29343d7c14a8ea27e639aa070b0a1c2815765f1b
Nick Telsan <nick@telsanstudios.com>
2022-09-25 20:53:40 -0400

    [#11] Navbar
    - Adds very basic navbar with log out

---
+++ web/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import { Container, IconButton, Text } from '@chakra-ui/react'
+
+import { useAuth } from '@redwoodjs/auth'
+
+import { Logout } from '../Icons'
+
+export const Navbar = () => {
+  const { logOut } = useAuth()
+
+  return (
+    <Container
+      w="100%"
+      mb="4px"
+      borderBottomColor="blackAlpha.700"
+      borderBottomWidth="1px"
+      borderBottomStyle="solid"
+      bgColor="whiteAlpha.300"
+      backdropBlur="2xl"
+      py="2px"
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Text fontSize="xl" fontWeight="bold">
+        Tasc
+      </Text>
+      <IconButton
+        aria-label="Logout"
+        size="sm"
+        variant="ghost"
+        p="0"
+        onClick={logOut}
+        icon={<Logout boxSize="1.5rem" />}
+      />
+    </Container>
+  )
+}
